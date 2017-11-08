ed1ac15179323ef91c60c0dc4cc12bc07df12a07
Katja Seeliger <kateiyas@users.noreply.github.com>
2017-11-08 13:59:52 +0100

    Update args.py

--- dcgan_recon/args.py
+++ dcgan_recon/args.py
@@ -2,20 +2,51 @@
 import argparse
 import numpy as np
 
-modeldir = 'model64x64_circle'
-
-# Arguments (TODO: improve this, create args object in first place)
+# NOTE: better to create args object in the first place
 parser = argparse.ArgumentParser()
 args = parser.parse_args()
 
 args.debug = False
 
 args.base_dir = '/vol/ccnlab-scratch1/katmul/reconv/'
-args.out_dir = args.base_dir + 'reconstruct_vim1'
-args.stimulus_path = args.base_dir + 'gentrain_imgs/'
-args.model_dir = "/vol/ccnlab-scratch1/katmul/reconv/gan_models/" + modeldir + '/'
 
 args.stimuli = 'vim1'   # horikawa || brains || vim1
+args.subject = 'Hyper'  # 1 || 2 || 3 || Hyper
+
+if args.stimuli == 'vim1':
+    modeldir = 'model64x64_circle'  # never use the masked GAN. the mask is applied within the model. 
+    args.bold_dir = '/vol/ccnlab-scratch1/GroupData/VIM-1/newVIM1/'
+    args.out_dir = args.base_dir + 'reconstruct_final_vim1'
+    args.stimulus_file = args.base_dir + 'vim1_stimuli_mat/vim1_stimuli_128.mat'
+    args.convert_grayscale  = False
+
+    args.calc_pca = True  # whether to do PCA on voxel responses (or use PCA'd data)
+    args.pcavar = 0.9
+    
+    args.load_pca = False
+    args.normalize = False   # vim-1 are already z-scored, plus makes no sense to place bold feature on same scale
+
+elif args.stimuli == 'horikawa':
+    modeldir = 'model64x64bwnomask'
+    args.out_dir = args.base_dir + 'reconstruct_horikawa'
+    #args.out_dir = args.base_dir + 'reconstruct_final_hori'
+    args.bold_dir = args.base_dir + 'horikawadata/'
+    args.stimulus_file = args.base_dir + 'horikawadata/stimuli_S1.mat'
+    args.convert_grayscale  = True  # convert to grayscale and increase image contrast
+
+    args.calc_pca = False  # whether to do PCA on voxel responses (or use PCA'd data)
+    args.load_pca = True
+
+    args.normalize = True   # vim-1 are already z-scored, plus makes no sense to place bold feature on same scale
+
+elif args.stimuli == 'brains':
+    modeldir = 'modelbrains'
+
+# Weight decay params
+args.do_weightdecay = True   # leads to 0-image in combination with MLP?
+args.regularization_alpha = 0.001
+
+args.model_dir = args.base_dir + "gan_models/" + modeldir + '/'
 
 args.seed = np.random.randint(1000)
 
@@ -29,28 +60,15 @@ args.apply_kaymask = True if args.stimuli == 'vim1' else False
 
 args.ndim_z = 50  # dimension of random z vector
 
-args.subject = '1'  # 1 || 2
-
 args.nepochs = 300 if not args.debug else 3
 
 args.nbatch = 3  # smaller batch size is better
 
-args.nrecon = 40  # must be multiple of 5, numbers of validation set images to reconstruct
+args.nrecon = 50  # must be multiple of 5, numbers of validation set images to reconstruct
 
-args.val_repetitions = 2  # you can repeat the remainder of the validation set for training
+args.val_repetitions = 1  # you can repeat the remainder of the validation set for training
 
-# Weight decay params
-args.do_weightdecay = False
-args.regularization_alpha = 0.0001
-
-args.bold_to_z_model = 'MLP'  # LinearRegression || MLP
-args.nhidden_mlp = 200
+args.bold_to_z_model = 'MLP'  # LinearRegression || MLP   # The MLP tends to overfit on train. 
+args.nhidden_mlp = 300
 
 args.only_earlyVC = False  # whether to focus on specific ROIs
-
-# TODO: reconsider: 
-# normalized_featurematching = False  # whether to normalize features for feature matching at each step
-
-args.do_pca = True  # whether to do PCA on voxel responses
-
-
